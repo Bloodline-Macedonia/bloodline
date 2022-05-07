837fd82729da2d52efaf9b363bd4a528b35e78c4
Yasir Idriz <yasiridriz@gmail.com>
2022-05-07 23:27:05 +0200

    cmon

--- components/layout/index.js
+++ components/layout/index.js
@@ -9,14 +9,13 @@ const Layout = ({ children }) => (
       <title>
         Bloodline
       </title>
-      <meta name="description" content="Burimi më i madh i librave digjitale në gjuhën Shqipe." />
+      <meta name="description" content="Blood donation made easy." />
 
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
-      <meta name="google-site-verification" content="XMgr__FORNFgpyt4vRg0NlFaVWnfUlAwlZc_jLPeteI" />
       {/* Preview */}
-      <meta property="og:url" content="https://librariadigjitale.co/" />
+      <meta property="og:url" content="https://bloodlinemacedonia.co/" />
       <meta property="og:type" content="website" />
       <meta
         property="og:title"
@@ -29,9 +28,7 @@ const Layout = ({ children }) => (
       <meta name="twitter:card" content="summary" />
       <meta property="og:image" content="https://i.postimg.cc/G2dtq6bP/logo.png" />
     </Head>
-    <Menu />
     {children}
-    <Footer />
   </>
 );
 
