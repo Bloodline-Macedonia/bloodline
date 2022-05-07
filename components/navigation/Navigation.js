fc8ee6e82492cdffb8b639448ca5fd4ca92325d4
Yasir Idriz <yasiridriz@gmail.com>
2022-05-07 23:28:54 +0200

    wtf

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -1,20 +1,21 @@
 import styles from "./Navigation.module.css";
+import Link from 'next/link';
 
 
 const Navigation = () => {
 	function handleScroll(e, id) {
-			e.preventDefault()
-			const element = document.getElementById(id)
-			element && element.scrollIntoView({behavior: "smooth"})
-			console.log(element)
-            
+		e.preventDefault()
+		const element = document.getElementById(id)
+		element && element.scrollIntoView({ behavior: "smooth" })
+		console.log(element)
+
 	}
 	return <div className={styles.container}>
-			<nav className={styles.nav}>
-					<a href="/" onClick={(e) => handleScroll(e,'about')}
-					> About Us </a>
-			</nav>
-		   </div>;
+		<nav className={styles.nav}>
+			<Link href={'/'}><a href="/" onClick={(e) => handleScroll(e, 'about')}
+			> About Us </a></Link>
+		</nav>
+	</div>;
 };
 
 export default Navigation;
