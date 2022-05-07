382651ba89a3c833c1c9e5652d1a390f32e9ec1b
Yasir Idriz <yasiridriz@gmail.com>
2022-05-07 23:30:18 +0200

    omg

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -3,17 +3,9 @@ import Link from 'next/link';
 
 
 const Navigation = () => {
-	function handleScroll(e, id) {
-		e.preventDefault()
-		const element = document.getElementById(id)
-		element && element.scrollIntoView({ behavior: "smooth" })
-		console.log(element)
-
-	}
 	return <div className={styles.container}>
 		<nav className={styles.nav}>
-			<Link href={'/'}><a href="/" onClick={(e) => handleScroll(e, 'about')}
-			> About Us </a></Link>
+			<Link href={'/'}><a> About Us </a></Link>
 		</nav>
 	</div>;
 };
